d96ae09e2f3c738e7f6d3002239c2f7f4f8eb3ba
Cody Banks <codydouglasbanks@gmail.com>
2022-02-20 18:04:38 -0500

    removal of conn string for security

--- Net6API/Models/User.cs
+++ Net6API/Models/User.cs
@@ -4,12 +4,12 @@ namespace Net6API.Models;
 
 public class User
 {
+    public Guid ID { get; set; }
     [Required]
     string UserName { get; set; }
     [Required]
     public string Password { get; set; }
-    public string Email { get; set; }
-    public Guid ID { get; set; }
+    public string Email { get; set; }    
     public string? CreatedBy { get; set; }
     public DateTime? CreatedOnDate { get; set; }
     public string? LastUpdatedBy { get; set; }
