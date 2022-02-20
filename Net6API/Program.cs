7b832f7bcc3610b22ba1ec4e45970d35f0ebc2ed
Cody Banks <codydouglasbanks@gmail.com>
2022-02-20 18:36:52 -0500

    added swagger personal info / desc

--- Net6API/Program.cs
+++ Net6API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Net6API.Data;
 using Net6API.Repository;
 
@@ -14,7 +15,20 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "V - 1.0.0",
+        Title = "Net6 Practice API",
+        Description = "An ASP.NET Core Web API for personal practice to implement new ideas",       
+        Contact = new OpenApiContact
+        {
+            Name = "Cody Banks",
+            Url = new Uri("https://example.com/contact")
+        }        
+    });
+});
 
 var app = builder.Build();
 
